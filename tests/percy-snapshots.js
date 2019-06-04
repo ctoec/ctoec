a0166ee9af52adc00f4cad266265de0232df4661
Gabriel Ramirez <gabriel.e.ramirez@gmail.com>
2019-06-04 11:10:38 -0500

    Setting up Percy

---
+++ tests/percy-snapshots.js
@@ -0,0 +1,10 @@
+const PercyScript = require('@percy/script');
+
+PercyScript.run(async (page, percySnapshot) => {
+  await page.goto('https://beta.ctoec.org');
+  await percySnapshot('homepage');
+
+  await page.goto('https://beta.ctoec.org/learn-more/');
+  await percySnapshot('learn-more');
+  
+});
