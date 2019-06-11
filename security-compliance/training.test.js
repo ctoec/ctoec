94a534f9edd46912e6b1700cb4fa847a2c8be6dd
Gabriel Ramirez <gabriel.e.ramirez@gmail.com>
2019-06-11 16:36:01 -0500

    Add a way of verifying the Emergency Plan has been reviewed recently

--- security-compliance/training.test.js
+++ security-compliance/training.test.js
@@ -1,14 +1,16 @@
+const moment = require('moment')
+
 describe('HIPAA Training', () => {
     test('is current for all project memebers', () => {
         const members = [
             {name: 'Gabriel Ramirez', training_date: new Date(2019, 5, 31), role: "Developer"},
         ]
 
-        const lastYear = new Date();
-        lastYear.setFullYear(lastYear.getFullYear() - 1);
+        const lastYearToday = moment().subtract(1, 'years');
 
         members.forEach(member => {
-            expect(member.training_date > lastYear, `${member.name}'s training is expired`).toBe(true)
+            expect(member.training_date > lastYearToday, `${member.name}'s training is expired`).toBe(true)
         })
     })
-});
+})
+
