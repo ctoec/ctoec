09df7506af6822c36f57f4a69967f4164651a4fa
Gabriel Ramirez <gabriel.e.ramirez@gmail.com>
2019-06-12 14:57:48 -0500

    Adding documentation for compliance for system logging

---
+++ security-compliance/system-logging.test.js
@@ -0,0 +1,25 @@
+const fs = require('fs')
+const path = require("path")
+const moment = require('moment')
+
+describe('System logs', () => {
+    test('have been reviewed recently', async () => {
+        const pathToSystemLoggingDocs = path.resolve('./security-compliance/SYSTEM_LOGGING.md')
+        const regex = /Application logs: Last reviewed - (\d{4}-\d{1,2}-\d{1,2})/ig;
+        const fileText = getFileText(pathToSystemLoggingDocs)
+        const dateMatch = regex.exec(fileText);
+        const lastReviewDate = new Date(dateMatch[1])
+        const threeMonthsAgo = moment().subtract(3, 'months');
+
+        expect(lastReviewDate > threeMonthsAgo, `System logs have not been reviewed for over 3 months`).toBe(true)
+
+    })
+})
+
+function getFileText(pathToSystemLoggingDocs) {
+    try {
+        return fs.readFileSync(pathToSystemLoggingDocs, 'utf8')
+    } catch (err) {
+        console.error(err)
+    }
+}
