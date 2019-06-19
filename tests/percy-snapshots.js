ab01a4ef705e87805f8ed99209447b4669d242d9
Gabriel Ramirez <gabriel.e.ramirez@gmail.com>
2019-06-19 15:39:43 -0500

    Setup deployments to staging env and check visuals before production deploys

--- tests/percy-snapshots.js
+++ tests/percy-snapshots.js
@@ -1,12 +1,12 @@
 const PercyScript = require('@percy/script');
 
 PercyScript.run(async (page, percySnapshot) => {
-  await page.goto('https://beta.ctoec.org');
+  await page.goto('http://stage.ctoec.org');
   await percySnapshot('homepage');
 
-  await page.goto('https://beta.ctoec.org/learn-more/');
+  await page.goto('http://stage.ctoec.org/learn-more/');
   await percySnapshot('learn-more');
 
-  await page.goto('https://beta.ctoec.org/licensing/');
+  await page.goto('http://stage.ctoec.org/licensing/');
   await percySnapshot('licensing');
 });
